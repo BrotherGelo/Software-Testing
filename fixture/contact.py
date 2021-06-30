b16d2f4bba5147cf1b9b4af3252bcf1e807be967
BrotherGelo <61095566+BrotherGelo@users.noreply.github.com>
2021-06-30 18:00:40 +0300

    Create classes Contact and Group (decomposition)

---
+++ fixture/contact.py
@@ -0,0 +1,54 @@
+from selenium.webdriver.support.ui import Select
+
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def init_creation_of_new_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def create(self, contact):
+        wd = self.app.wd
+        self.init_creation_of_new_contact()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        self.app.return_to_home_page()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,7 +13,7 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_new_contact(Contact(firstname="Vasya", middlename="Vasiylyevich", lastname="Pupkin", nickname="Vasyan3000",
-                                   title="SYPER VASYA", company="international", address="bassejnaya street", home="homeless",
-                                   mobile="8800553535", email="vasyanT1000@vasya.ru", bday="10", bmonth="September", byear="1901"))
+    app.contact.create(Contact(firstname="Vasya", middlename="Vasiylyevich", lastname="Pupkin", nickname="Vasyan3000",
+                               title="SYPER VASYA", company="international", address="bassejnaya street", home="homeless",
+                               mobile="8800553535", email="vasyanT1000@vasya.ru", bday="10", bmonth="September", byear="1901"))
     app.session.logout()
