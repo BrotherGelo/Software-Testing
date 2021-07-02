7a6612da62592e4a1fb749d042fcef1090f5dc97
BrotherGelo <61095566+BrotherGelo@users.noreply.github.com>
2021-07-02 14:54:56 +0300

    Повышение надёжности тестов
    Реализована валидация фикстуры перед тестами
    Доработан SessionHelper

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,4 +5,3 @@ def test_modify_contact(app):
     app.contact.modify_first_contact(Contact(firstname="Vasiliy", middlename="Petrovich", lastname="Pupkin", nickname="SuperPupkin",
                                title="SYPER VASYA", company="Umbrella inc", address="bassejnaya street", home="23",
                                mobile="8800553535", email="vasyanT1000@vasya.ru", bday="11", bmonth="December", byear="1984"))
-    app.session.logout()
