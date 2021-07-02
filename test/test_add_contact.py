bb2ffcabfd7d1ac5ae4acd7133ea3088f783ea5a
BrotherGelo <61095566+BrotherGelo@users.noreply.github.com>
2021-07-02 13:30:37 +0300

    mod login&logout
    Перенёс логин и логаут в conftest

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,8 +3,6 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Vasya", middlename="Vasiylyevich", lastname="Pupkin", nickname="Vasyan3000",
                                title="SYPER VASYA", company="international", address="bassejnaya street", home="homeless",
                                mobile="8800553535", email="vasyanT1000@vasya.ru", bday="10", bmonth="September", byear="1901"))
-    app.session.logout()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,7 +2,6 @@ from model.contact import Contact
 
 
 def test_modify_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="Vasiliy", middlename="Petrovich", lastname="Pupkin", nickname="SuperPupkin",
                                title="SYPER VASYA", company="Umbrella inc", address="bassejnaya street", home="23",
                                mobile="8800553535", email="vasyanT1000@vasya.ru", bday="11", bmonth="December", byear="1984"))
