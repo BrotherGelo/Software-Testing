ee2bb8f92fdd031e3645f9358ce43b76b8785395
BrotherGelo <61095566+BrotherGelo@users.noreply.github.com>
2021-07-03 17:03:37 +0300

    Check group list change
    Добавлена проверка на создание/изменение/удаление групп

--- model/group.py
+++ model/group.py
@@ -1,6 +1,7 @@
 class Group:
 
-    def __init__(self, name=None, header=None, footer=None):
+    def __init__(self, name=None, header=None, footer=None, id=None):
         self.name = name
         self.footer = footer
         self.header = header
+        self.id = id
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,9 +4,15 @@ from model.group import Group
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test name to delete"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="TEST GROUP NAME"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(header="test header to delete"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="TEST GROUP HEADER"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
