dd488e82d5553ecee98dddebda6da7275f93823b
BrotherGelo <61095566+BrotherGelo@users.noreply.github.com>
2021-07-07 01:01:45 +0300

    Task №11 (update)
    Реализована проверка тестов в виде сравнения списков для всех тестов.
    
    Исправил тест для модификации контактов.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,14 +3,14 @@ from model.contact import Contact
 
 def test_modify_contact(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(title="TEST TITLE FOR DELETE"))
-    contact = (Contact(firstname="Vasiliy", middlename="Petrovich",
+        app.contact.create(Contact(lastname="TEST TITLE FOR MODIFY"))
+    contact = (Contact(firstname="Vasiliy", lastname="Kamyshkin", middlename="Petrovich",
                        bday="11", bmonth="December", byear="1984"))
     old_contacts = app.contact.get_contact_list()
     contact.id = old_contacts[0].id
     app.contact.modify_first_contact(contact)
-    contact.firstname = old_contacts[0].firstname
-    contact.lastname = old_contacts[0].lastname
+    #contact.firstname = old_contacts[0].firstname
+    #contact.lastname = old_contacts[0].lastname
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
