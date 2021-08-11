e110be6e1db1cb4f8ac173b6af99a8d87faa2908
BrotherGelo <brothergelo@gmail.com>
2021-08-11 14:21:41 +0300

    21 Задание есть вопросы

--- test/test_fields_contact.py
+++ test/test_fields_contact.py
@@ -1,12 +1,41 @@
 import re
-from random import randrange
 from model.contact import Contact
 
 
+def check_empty_list(app, db):
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(
+            Contact(firstname="TEST_Vasya", middlename="TEST_Vasiylyevich", lastname="TEST_Pupkin", nickname="TEST_Vasyan3000",
+                    title="TEST_SYPER VASYA", company="TEST_international", address="TEST_bassejnaya street",
+                    homephone="8-626-256-27-27", workphone="+7(777)227 27 27", mobilephone="8(800)555-35-35",
+                    secondaryphone="682828", email="TEST_vasyanT1000@vasya.ru", email2="TEST_vasyanT1001@vasya.ru",
+                    email3="TEST_vasyanT1002@vasya.ru", bday="10", bmonth="September", byear="1901"))
+
+
+def test_compare_all_fields(app, db):
+    check_empty_list(app, db)
+    list_ui = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    list_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    for i in range(len(list_db)):
+        contact_ui = list_ui[i]
+        contact_db = list_db[i]
+        a = contact_ui.address
+        b = contact_db.address
+        c = contact_ui.all_phones_from_home_page
+        d = merge_phones_like_on_home_page(contact_db)
+        e = contact_ui.all_emails_from_home_page
+        f = merge_emails_like_on_home_page(contact_db)
+        assert contact_ui.id == contact_db.id
+        assert contact_ui.firstname == contact_db.firstname
+        assert contact_ui.lastname == contact_db.lastname
+        assert contact_ui.address == contact_db.address
+        assert contact_ui.all_phones_from_home_page == merge_phones_like_on_home_page(contact_db)
+        assert contact_ui.all_emails_from_home_page == merge_emails_like_on_home_page(contact_db)
+
+"""
 def test_compare_firstname_on_home_page(app):
     contact_from_edit_page, contact_from_home_page = preconditions_home_page(app)
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-
 
 
 def test_compare_lastname_on_home_page(app):
@@ -38,7 +67,14 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_view_page.workphone == contact_from_edit_page.workphone
     assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
     assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
-
+    
+def preconditions_home_page(app):
+    check_empty_list(app)
+    index = randrange(len(app.contact.get_contact_list()))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    return contact_from_edit_page, contact_from_home_page
+"""
 
 def clear(s):
     return re.sub("[() -]", "", s)
@@ -57,20 +93,5 @@ def merge_emails_like_on_home_page(contact):
                             filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3])))
 
 
-def check_empty_list(app):
-    if app.contact.count() == 0:
-        app.contact.create(
-            Contact(firstname="TEST_Vasya", middlename="TEST_Vasiylyevich", lastname="TEST_Pupkin", nickname="TEST_Vasyan3000",
-                    title="TEST_SYPER VASYA", company="TEST_international", address="TEST_bassejnaya street",
-                    homephone="8-626-256-27-27", workphone="+7(777)227 27 27", mobilephone="8(800)555-35-35",
-                    secondaryphone="682828", email="TEST_vasyanT1000@vasya.ru", email2="TEST_vasyanT1001@vasya.ru",
-                    email3="TEST_vasyanT1002@vasya.ru", bday="10", bmonth="September", byear="1901"))
 
-
-def preconditions_home_page(app):
-    check_empty_list(app)
-    index = randrange(len(app.contact.get_contact_list()))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    return contact_from_edit_page, contact_from_home_page
 
