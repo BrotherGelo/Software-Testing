f6ca1e7ce7d7d5265d5ef75b2cfa235250802093
BrotherGelo <brothergelo@gmail.com>
2021-08-12 15:12:38 +0300

    Задание №20. Доработка
    Добавил заполнение id

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,19 +4,20 @@ import random
 
 def check_empty_contact_list(app, db):
     if len(db.get_contact_list()) == 0:
-        app.contact.create(Contact(title="TEST TITLE FOR MODIFY"))
+        app.contact.create(Contact(firstname="Vasiliy", lastname="Kamyshkin", middlename="Petrovich",
+                       bday="11", bmonth="December", byear="1984"))
 
 
 def test_modify_random_contact(app, db, check_ui):
     check_empty_contact_list(app, db)
-    mod_contact = (Contact(firstname="Vasiliy", lastname="Kamyshkin", middlename="Petrovich",
-                       bday="11", bmonth="December", byear="1984"))
+    mod_contact = (Contact(title="TEST TITLE FOR MODIFY"))
     old_contacts = db.get_contact_list()
     contact = random.choice(old_contacts)
     index = old_contacts.index(contact)
     app.contact.modify_contact_by_id(contact.id, mod_contact)
     new_contacts = db.get_contact_list()
     old_contacts[index] = mod_contact
+    old_contacts[index].id = new_contacts[index].id
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(old_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
